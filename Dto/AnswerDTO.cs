c44b7b4233836a2203c55ed80649d3e08806f5a3
abdellah098 <saley.abdellah@gmail.com>
2022-01-04 09:52:33 +0100

    [feat] modélisation des DTO des questions et réponses

---
+++ Dto/AnswerDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Quiz_back.Dto
+{
+    public class AnswerDTO
+    {
+        public Guid  Id { get; set; }
+        public string Text { get; set; }
+        public bool isCorrect { get; set; }
+        public bool isNew { get; set; }
+    }
+}
---
+++ Dto/QuestionDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Quiz_back.Dto
+{
+    public class QuestionDto
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public bool  isNew { get; set; }
+        public List<AnswerDTO> Answers { get; set; }
+
+        public QuestionDto()
+        {
+            Answers = new List<AnswerDTO>();
+        }
+    }
+}
